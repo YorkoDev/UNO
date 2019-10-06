c811e9d5e9561f392bafdf25f740ec3e84d099cf
JuanchoElPu <41529106+JuanchoElPu@users.noreply.github.com>
2019-10-06 18:08:10 -0300

    update pipe.c
    crear arreglo con pids

--- pipes.c
+++ pipes.c
@@ -10,94 +10,110 @@
 #include <string.h>
 #include <dirent.h>
 
+int id;
+int turnos[4];
 
+
+//[padre,hijo1.hijo2,hijo3]
 int creacionprocesos(){
-	int i;
-	i = fork();
-	if(i < 0){
-		return -1;
-	}
-	else if(i > 0){
-		i = fork();
-		if(i < 0){
-			return -2;
-		}
-		else if(i > 0){
-			i = fork();
-			if(i < 0){
-				return -3;
-			}
-			return 1;	
-		}
-		else{
-			return 1;
-		}
-	}
-	else{
-		return 1;
-	}
+    int i;
+    turnos[0]= getpid();
+    i = fork();
+    if(i < 0){
+        return -1;
+    }
+    else if(i > 0){
+        turnos[1]= i;
+        i = fork();
+        if(i < 0){
+            return -2;
+        }
+        else if(i > 0){
+            turnos[2]=i;
+            i = fork();
+            if(i < 0){
+                return -3;
+            }
+            else if(i > 0) turnos[3] = i;
+            else turnos[3] = getpid();
+            return 1;   
+        }
+        else{
+            turnos[3] = -1;
+            turnos[2] = getpid();
+            return 1;
+        }
+    }
+    else{
+        turnos[3] = -1;
+        turnos[2] = -1;
+        turnos[1] = getpid();
+        return 1;
+    }
 }
 
 
 int main(){
-	int PH[2]; //pipe padre a hijo
-	int HP[2]; //hijo a padre
+    int PH[2]; //pipe padre a hijo
+    int HP[2]; //hijo a padre
 
-	char fixed_str[] = "ayuda";
-	char input_str[100];
+    char fixed_str[] = "ayuda";
+    char input_str[100];
 
-	int p;
+    int p;
 
-	if(pipe(PH) == -1){
-		printf("Creo que algo salio mal\n");
-		exit(1);
-	}
-	if(pipe(HP) == -1){
-		printf("Creo que algo salio mal\n");
-		exit(1);
-	}
+    if(pipe(PH) == -1){
+        printf("Creo que algo salio mal\n");
+        exit(1);
+    }
+    if(pipe(HP) == -1){
+        printf("Creo que algo salio mal\n");
+        exit(1);
+    }
 
-	scanf("%s",input_str);
-	p = fork();
+    scanf("%s",input_str);
+    p = fork();
 
-	if(p < 0){
-		printf("Fork fallo\n");
-	}
-	else if(p > 0){
-		char concat_str[100];
+    if(p < 0){
+        printf("Fork fallo\n");
+    }
+    else if(p > 0){
+        char concat_str[100];
 
-		close(PH[0]);
+        close(PH[0]);
 
-		write(PH[1],input_str,strlen(input_str)+1);
-		close(PH[1]);
+        write(PH[1],input_str,strlen(input_str)+1);
+        close(PH[1]);
 
-		wait(NULL);
+        wait(NULL);
 
-		close(HP[1]);
+        close(HP[1]);
 
-		read(HP[0],concat_str,100);
-		printf("Concatenated string %s\n",concat_str);
-		close(HP[0]);
-	}
-	else{
-		close(PH[1]);
+        read(HP[0],concat_str,100);
+        printf("Concatenated string %s\n",concat_str);
+        close(HP[0]);
+    }
+    else{
+        close(PH[1]);
 
-		char concat_str[100];
-		read(PH[0],concat_str,100);
+        char concat_str[100];
+        read(PH[0],concat_str,100);
 
-		int k = strlen(concat_str);
-		int i;
-		for(i = 0; i < strlen(fixed_str);i++) concat_str[k++] = fixed_str[i];
-		concat_str[k] = '\0';
-		
-		close(HP[0]);
-		close(PH[0]);
+        int k = strlen(concat_str);
+        int i;
+        for(i = 0; i < strlen(fixed_str);i++) concat_str[k++] = fixed_str[i];
+        concat_str[k] = '\0';
+        
+        close(HP[0]);
+        close(PH[0]);
 
-		write(HP[1],concat_str,strlen(concat_str)+1);
-		close(HP[1]);
-		exit(1);
-	}
-	p = creacionprocesos();
-	printf("media vola\n");
-	return 0;
+        write(HP[1],concat_str,strlen(concat_str)+1);
+        close(HP[1]);
+        exit(1);
+    }
+    p = creacionprocesos();
+    printf("[");
+    for(p = 0; p < 4-1; p++) printf("%d,",turnos[p]);
+    printf("%d]\n",turnos[p]);
+    return 0;
 }
